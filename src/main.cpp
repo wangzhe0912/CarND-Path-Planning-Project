19d5a6a3eb65dc70077cdafbc4765ecaa8cfe174
wangzhe12 <wangzhe12@baidu.com>
2018-12-08 18:52:30 +0800

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -243,11 +243,6 @@ int main() {
 
           	int prev_size = previous_path_x.size();
 
-          	json msgJson;
-
-          	vector<double> next_x_vals;
-          	vector<double> next_y_vals;
-
 
 
           	vector<double> ptsx;
@@ -275,23 +270,76 @@ int main() {
 
                 double ref_x_prev = previous_path_x[prev_size - 2];
                 double ref_y_prev = previous_path_y[prev_size - 2];
-                
+                ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+                ptsx.push_back(ref_x_prev);
+                ptsx.push_back(ref_x);
+
+                ptsy.push_back(ref_y_prev);
+                ptsy.push_back(car_y);
+            }
+
+            vector<double> next_wp0 = getXY(car_s + 30, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp1 = getXY(car_s + 60, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s + 90, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+            ptsx.push_back(next_wp0[0]);
+            ptsx.push_back(next_wp1[0]);
+            ptsx.push_back(next_wp2[0]);
+
+            ptsy.push_back(next_wp0[1]);
+            ptsy.push_back(next_wp1[1]);
+            ptsy.push_back(next_wp2[1]);
+
+
+            for (int i = 0; i < ptsx.size(); i++) {
+                double shift_x = ptsx[i] - ref_x;
+                double shift_y = ptsy[i] - ref_y;
+
+                ptsx[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
+                ptsy[i] = shift_x * cos(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
             }
 
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+            tk::spline s;
+            s.set_points(ptsx, ptsy);
+            vector<double> next_x_vals;
+            vector<double> next_y_vals;
 
-            double dist_inc = 0.3;
-            for(int i = 0; i < 50; i++)
-            {
-                double next_s = car_s + (i + 1) * dist_inc;
-                double next_d = 6;
 
-                vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-
-                next_x_vals.push_back(xy[0]);
-                next_y_vals.push_back(xy[1]);
+            for (int i = 0; i < previous_path_x.size(); i++) {
+                next_x_vals.push_back(previous_path_x[i]);
+                next_y_vals.push_back(previous_path_y[i]);
             }
+
+            double target_x = 30.0;
+            double target_y = s(target_x);
+
+            double target_dist = sqrt(target_x * target_x + target_y * target_y);
+            double x_add_on = 0;
+
+            for (int i = 1; i <= 50 - previous_path_x.size(); i++) {
+                double N = target_dist / (0.02 * ref_vel / 2.24);
+                double x_point = x_add_on + target_x / N;
+                double y_point = s(x_point);
+
+                x_add_on = x_point;
+
+                double x_ref = x_point;
+                double y_ref = y_point;
+
+                x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+                y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+                x_point += ref_x;
+                y_point += ref_y;
+
+                next_x_vals.push_back(x_point);
+                next_y_vals.push_back(y_point);
+
+            }
+
+            json msgJson;
 
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
