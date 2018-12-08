18f78bea23ec6650cb8c413fb338b8abc0fd33b8
王哲 <wangzhe@wangzhedeMini.lan>
2018-12-08 17:37:21 +0800

    first commit

---
+++ test.sh
@@ -0,0 +1,6 @@
+cd /Users/wangzhe/Desktop/CarND-Path-Planning-Project
+./clean.sh
+cd /Users/wangzhe/Desktop/CarND-Path-Planning-Project
+./build.sh
+cd /Users/wangzhe/Desktop/CarND-Path-Planning-Project
+./run.sh
